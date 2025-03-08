fe44cb690e2ed96f5fb4451e7c50602dd2505e59
iqbolshoh <iilhomjonov777@gmail.com>
2025-03-08 11:33:00 +0500

    commit

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Send Email</title>
+    <link rel="icon" href="https://iqbolshoh.uz/favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
